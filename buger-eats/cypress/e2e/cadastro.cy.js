feec016683e3d8158779c57339300d120fbb0925
Eduardo Zirbell <eduardozirbell26@gmail.com>
2022-06-13 21:14:47 -0300

    Abrindo cadastro de entregadores

--- buger-eats/cypress/e2e/cadastro.cy.js
+++ buger-eats/cypress/e2e/cadastro.cy.js
@@ -2,6 +2,7 @@ describe('Cadastro', () => {
   it('Usuário deve se tornar um entregador', () => {
     cy.viewport(1920, 1080)
     cy.visit('https://buger-eats.vercel.app/?_gl=1*1nkq6f4*_ga*MTAxNTU4NzQyNy4xNjU0MTk1MjQ4*_ga_37GXT4VGQK*MTY1NDE5NTMzOC4xLjEuMTY1NDE5NjIxNy4w')
-    .click('#page-home main a')
+    cy.get('a[href="/deliver"]').click()
+    cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
   })
 })
