e5fed6cdc8fe9e6a81e11c7b198a5119a51af170
Eduardo Zirbell <eduardozirbell26@gmail.com>
2022-06-21 21:09:15 -0300

    Page objective

--- buger-eats/cypress/e2e/cadastro.cy.js
+++ buger-eats/cypress/e2e/cadastro.cy.js
@@ -1,9 +1,7 @@
+import SignupPage from "../pages/SignupPage"
+var signuppage = new SignupPage()
 describe('Register', () => {
   it('User must become deliver', () => {
-    cy.viewport(1920, 1080)
-    cy.visit('https://buger-eats.vercel.app/?_gl=1*1nkq6f4*_ga*MTAxNTU4NzQyNy4xNjU0MTk1MjQ4*_ga_37GXT4VGQK*MTY1NDE5NTMzOC4xLjEuMTY1NDE5NjIxNy4w')
-    cy.get('a[href="/deliver"]').click()
-    cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
     let deliver = {
       name: "Eduardo Zirbell",
       document: "10228297931",
@@ -20,34 +18,14 @@ describe('Register', () => {
         city_state: "Blumenau/SC"
       }
     }
-    cy.get('input[name="name"]').type(deliver.name)
-    cy.get('input[name="cpf"]').type(deliver.document)
-    cy.get('input[name="email"]').type(deliver.email)
-    cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
-    cy.get('input[name="postalcode"]').type(deliver.address.postal_code)
-    cy.get('input[type="button"][value="Buscar CEP"').click()
-    cy.get('input[name="address-number"]').type(deliver.address.number)
-    cy.get('input[name="address-details"]').type(deliver.address.details)
-    cy.get('input[name="address"]').should('have.value', deliver.address.street)
-    cy.get('input[name="district"]').should('have.value', deliver.address.district)
-    cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-    cy.contains('.delivery-method li', deliver.delivery_method).click()
-    // ^ serve para encontrar algo no início de uma propriedade
-    // $ serve para encontrar algo no fim de uma propriedade
-    // * serve para encontrar algo em qualquer lugar de uma propriedade
-    cy.get('input[accept="image/*"').attachFile(deliver.cnh)
-    cy.get('form button[type="submit"').click()
-
-    const expectedMessage = "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato."
-
-    cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
+    const expectedMessageSucess = "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato."
+    signuppage.go()
+    signuppage.fillForm(deliver)
+    signuppage.submit()
+    signuppage.modalContentShouldBe(expectedMessageSucess)
   })
 
   it('Document was incorrect', () => {
-    cy.viewport(1920, 1080)
-    cy.visit('https://buger-eats.vercel.app/?_gl=1*1nkq6f4*_ga*MTAxNTU4NzQyNy4xNjU0MTk1MjQ4*_ga_37GXT4VGQK*MTY1NDE5NTMzOC4xLjEuMTY1NDE5NjIxNy4w')
-    cy.get('a[href="/deliver"]').click()
-    cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
     let deliver = {
       name: "Eduardo Zirbell",
       document: "A32031B032A",
@@ -64,24 +42,10 @@ describe('Register', () => {
         city_state: "Blumenau/SC"
       }
     }
-    cy.get('input[name="name"]').type(deliver.name)
-    cy.get('input[name="cpf"]').type(deliver.document)
-    cy.get('input[name="email"]').type(deliver.email)
-    cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
-    cy.get('input[name="postalcode"]').type(deliver.address.postal_code)
-    cy.get('input[type="button"][value="Buscar CEP"').click()
-    cy.get('input[name="address-number"]').type(deliver.address.number)
-    cy.get('input[name="address-details"]').type(deliver.address.details)
-    cy.get('input[name="address"]').should('have.value', deliver.address.street)
-    cy.get('input[name="district"]').should('have.value', deliver.address.district)
-    cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-    cy.contains('.delivery-method li', deliver.delivery_method).click()
-    // ^ serve para encontrar algo no início de uma propriedade
-    // $ serve para encontrar algo no fim de uma propriedade
-    // * serve para encontrar algo em qualquer lugar de uma propriedade
-    cy.get('input[accept="image/*"').attachFile(deliver.cnh)
-    cy.get('form button[type="submit"').click()
-
-    cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+    const expectedMessageDocument = "Oops! CPF inválido"
+    signuppage.go()
+    signuppage.fillForm(deliver)
+    signuppage.submit()
+    signuppage.alertDocumentShouldBe(expectedMessageDocument)
   })
 })
---
+++ buger-eats/cypress/pages/SignupPage.js
@@ -0,0 +1,41 @@
+class SignupPage{
+    go(){
+        cy.viewport(1920, 1080)
+        cy.visit('https://buger-eats.vercel.app/?_gl=1*1nkq6f4*_ga*MTAxNTU4NzQyNy4xNjU0MTk1MjQ4*_ga_37GXT4VGQK*MTY1NDE5NTMzOC4xLjEuMTY1NDE5NjIxNy4w')
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+    }
+    
+    fillForm(deliver){
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.document)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+        cy.get('input[name="postalcode"]').type(deliver.address.postal_code)
+        cy.get('input[type="button"][value="Buscar CEP"').click()
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
+        cy.get('input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
+        cy.contains('.delivery-method li', deliver.delivery_method).click()
+        // ^ serve para encontrar algo no início de uma propriedade
+        // $ serve para encontrar algo no fim de uma propriedade
+        // * serve para encontrar algo em qualquer lugar de uma propriedade
+        cy.get('input[accept="image/*"').attachFile(deliver.cnh)
+    }
+
+    submit(){
+        cy.get('form button[type="submit"').click()
+    }
+
+    modalContentShouldBe(expectedMessage){
+        cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
+    }
+
+    alertDocumentShouldBe(expectedMessage){
+        cy.get('.alert-error').should('have.text', expectedMessage)
+    }
+}
+
+export default SignupPage
