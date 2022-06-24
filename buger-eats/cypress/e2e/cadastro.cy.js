51614370c5c9d6c085256a226c0ce3a7653823bd
Eduardo Zirbell <eduardozirbell26@gmail.com>
2022-06-23 21:31:39 -0300

    Separação de variavéis

--- buger-eats/cypress/e2e/cadastro.cy.js
+++ buger-eats/cypress/e2e/cadastro.cy.js
@@ -1,50 +1,25 @@
 import SignupPage from "../pages/SignupPage"
 var signuppage = new SignupPage()
 describe('Register', () => {
-  it('User must become deliver', () => {
-    let deliver = {
-      name: "Eduardo Zirbell",
-      document: "10228297931",
-      email: "eduardozirbell26@gmail.com",
-      whatsapp: "47992570433",
-      delivery_method: "Bicicleta",
-      cnh: "images/cnh-digital.jpg",
-      address: {
-        postal_code: "89041760",
-        street: "Rua José Ramos",
-        number: "126",
-        details: ".",
-        district: "Salto Weissbach",
-        city_state: "Blumenau/SC"
-      }
-    }
+  beforeEach(function(){
+    cy.fixture('deliver').then((deliver)=>{
+      this.deliver = deliver
+    })
+  })
+  it('User must become deliver', function() {
+    
     const expectedMessageSucess = "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato."
     signuppage.go()
-    signuppage.fillForm(deliver)
+    signuppage.fillForm(this.deliver.signup)
     signuppage.submit()
     signuppage.modalContentShouldBe(expectedMessageSucess)
   })
 
-  it('Document was incorrect', () => {
-    let deliver = {
-      name: "Eduardo Zirbell",
-      document: "A32031B032A",
-      email: "eduardozirbell26@gmail.com",
-      whatsapp: "47992570433",
-      delivery_method: "Bicicleta",
-      cnh: "images/cnh-digital.jpg",
-      address: {
-        postal_code: "89041760",
-        street: "Rua José Ramos",
-        number: "126",
-        details: ".",
-        district: "Salto Weissbach",
-        city_state: "Blumenau/SC"
-      }
-    }
+  it('Document was incorrect', function() {
+
     const expectedMessageDocument = "Oops! CPF inválido"
     signuppage.go()
-    signuppage.fillForm(deliver)
+    signuppage.fillForm(this.deliver.documentInv)
     signuppage.submit()
     signuppage.alertDocumentShouldBe(expectedMessageDocument)
   })
--- buger-eats/cypress/pages/SignupPage.js
+++ buger-eats/cypress/pages/SignupPage.js
@@ -1,7 +1,7 @@
 class SignupPage{
     go(){
         cy.viewport(1920, 1080)
-        cy.visit('https://buger-eats.vercel.app/?_gl=1*1nkq6f4*_ga*MTAxNTU4NzQyNy4xNjU0MTk1MjQ4*_ga_37GXT4VGQK*MTY1NDE5NTMzOC4xLjEuMTY1NDE5NjIxNy4w')
+        cy.visit('https://buger-eats.vercel.app')
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
     }
