bd472aa2782dbda97f4cc9bead80d2940af835f1
Eduardo Zirbell <eduardozirbell26@gmail.com>
2022-07-29 11:32:39 -0300

    curso finalizado

--- alura/cypress/integration/3-ALURAPIC/api-alurapic.spec.js
+++ alura/cypress/integration/3-ALURAPIC/api-alurapic.spec.js
@@ -10,4 +10,20 @@ describe('buscar fotos e dados', ()=> {
             expect(resposta.body[0].description).to.be.equal('Farol iluminado')
         })
     })
+    it('fazer login do flavio', ()=> {
+        const tempoEsperado = Math.random() * 2000
+        cy.request({
+            method: 'POST',
+            url : 'https://apialurapic.herokuapp.com/user/login',
+            body : Cypress.env()
+        }).then((resposta) => {
+            expect(resposta.status).to.be.equal(200)
+            expect(resposta.body).is.not.empty
+            expect(resposta.body).to.have.property('id')
+            expect(resposta.body.id).to.be.equal(1)
+            expect(resposta.body).to.have.property('email')
+            expect(resposta.body.email).to.be.equal('flavio@alurapic.com.br')
+            expect(resposta.duration).to.be.lte(tempoEsperado)
+        })
+    })
 })
